f0f642e8f2b08a4d400b73925cb3455a56de8b2a
Hanife Erkalan <73136159+eliferkln@users.noreply.github.com>
2024-06-08 23:56:09 +0300

    Add: model viewer ✔️

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,54 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <div class="model">
+      <div class="model__view">
+        <model-viewer
+          id="first"
+          src="/assets/mickey.glb"
+          ios-src="/assets/mickey.usdz"
+          seamless-poster
+          environment-image="neutral"
+          exposure="1.0"
+          interaction-prompt-threshold="0"
+          shadow-intensity="1"
+          ar
+          autoplay
+          ar-modes="webxr scene-viewer quick-look"
+          auto-rotate
+          camera-controls
+          camera-orbit="0deg 90deg 0deg 8.37364m"
+          alt="3D model"
+          style="
+            display: flex;
+            align-items: center;
+            background-color: transparent;
+            margin: auto;
+          "
+        ></model-viewer>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import '@google/model-viewer'
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+.model {
+  margin-top: 35px;
+}
+.model__view model-viewer {
+  width: 20em;
+  height: 20em;
+}
+@media only screen and (min-width: 320px) and (max-width: 480px) {
+  .model__view model-viewer {
+    width: 15em;
+    height: 15em;
+  }
 }
 </style>
